b09a5af5f6138ae203129e5bcc6eb68e4c901d7a
Graeme Wilson <graeme@techlunacy.com>
2011-09-02 21:43:28 +1000

    no really this is the end... untill i get testy

---
+++ Tasks/Helpers/TaskHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tasks.Models;
+
+namespace Tasks.Helpers
+{
+    public class TaskHelper
+    {
+        public static List<SelectListItem> PriorityDropDownListPopulation()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            for (int i = TaskModel.LowestPriority; i <= TaskModel.HighestPriority; i++)
+            {
+                string textValue = i.ToString() + " ";
+                switch (i)
+                {
+                    case TaskModel.LowestPriority:
+                        textValue += "Lowest";
+                        break;
+                    case TaskModel.NormalPriority:
+                        textValue += "Normal";
+                        break;
+                    case TaskModel.HighestPriority:
+                        textValue += "Highest";
+                        break;
+                }
+                SelectListItem selectListItem = new SelectListItem { Text = textValue, Value = i.ToString(), Selected = (i == TaskModel.NormalPriority) };
+                items.Add(selectListItem);
+            }
+            return items;
+        }
+    }
+}
---
+++ Tasks/Views/Shared/EditorTemplates/PriorityDropDown.ascx
@@ -0,0 +1,3 @@
+﻿<%@ Control Language="C#" Inherits="System.Web.Mvc.ViewUserControl" %>
+<%@ Import Namespace="Tasks.Helpers" %>
+<%: Html.DropDownList("", TaskHelper.PriorityDropDownListPopulation()) %>
